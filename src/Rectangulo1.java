5da51c3f192ab1620592e430caf3c741e50dc1d9
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-30 19:47:20 -0300

    esta clase nos calcula la edad que tendremos en 20 años mas

---
+++ src/Rectangulo1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LAB04
+ */
+public class Rectangulo1
+{
+   public static void main(String[] args)
+   { 
+       
+int altura;
+int base;
+int area;
+Integer.parseInt(JOptionPane.showInputDialog("ingrese la base del rectangulo"));
+Integer.parseInt(JOptionPane.showInputDialog("ingrese la altura del rectangulo"));
+
+   }
+   
+       
+}
