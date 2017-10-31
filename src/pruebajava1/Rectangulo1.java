5e2f147eceed566b4f805e9d97765b25bf6e9c87
lilianaliray <marcelo_masoka@hotmail.com>
2017-10-30 21:01:10 -0300

    esta clase nos calcula el area de un regtangulo

---
+++ src/pruebajava1/Rectangulo1.java
@@ -0,0 +1,35 @@
+package pruebajava1;
+
+/************************************************************************
+ * autor= liliana hevia
+ * fecha= 30/10/2017
+ * nombre de la clase= rectangulo1
+ * responsabilidad= esta clase calcula el area del rectangulo
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author LAB04
+ */
+public class Rectangulo1
+{
+   public static void main(String[] args)
+   { 
+int areaRectangulo =0;       
+int baseRectangulo =5;
+int alturaRectangulo =4;
+
+
+areaRectangulo = baseRectangulo*alturaRectangulo;
+
+System.out.println("El area de un rectangulo de base "+baseRectangulo+" y altura "+alturaRectangulo+" es igual a " +areaRectangulo );
+
+   }
+   
+       
+}
